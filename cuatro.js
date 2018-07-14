022c05d9f9e8d4114b6f9a9b0c2b3baa7b5aeeba
ceagazzi <40808724+ceagazzi@users.noreply.github.com>
2018-07-13 22:11:51 -0300

    Switch

---
+++ cuatro.js
@@ -0,0 +1,14 @@
+function mostrar()
+{
+	var numero1;
+	var numero2;
+	var resultado;
+
+	prompt ("numero1");
+	prompt ("numero2");
+
+		if (numero1=numero2); {
+			alert("numero1" + "numero2");
+	}
+
+}
---
+++ jsInstruccionSwitch(4).js
@@ -0,0 +1,30 @@
+function mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+
+//alert (mesDelAño);
+	switch (mesDelAño)
+	{
+		case "Febrero":
+			alert ("Este mes tiene 28 dias");
+			break;
+
+		case "Enero":
+		case "Marzo":
+		case "Mayo":
+		case "Julio":
+		case "Agosto":
+		case "Octubre":
+		case "Diciembre":
+			alert ("Este mes tiene 31 dias");
+			break;
+
+		default:
+			alert ("Este mes tiene 30 dias");		
+	}
+	
+
+
+
+}//FIN DE LA FUNCIÓN
