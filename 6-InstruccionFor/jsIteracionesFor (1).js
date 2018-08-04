5dc3b0b810ae6d39116212c1007e73be3ec9b0ae
ceagazzi <40808724+ceagazzi@users.noreply.github.com>
2018-08-03 22:05:16 -0300

    Ejercicios For

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,49 @@
 function mostrar()
 {
+	/*var contador;
+
+	for(contador=0;contador<5;contador++)
+	{
+		console.log(contador);
+	}
+
+	console.log("final" + contador);*/
+	//LO DE ARRIBA FUNCIONA!!
+	/*var contador;
+
+	for(contador=0;contador<5;)
+	{
+		contador++
+		console.log(contador);
+	}
+
+	console.log("final" + contador);*/
+	//El BREAK ROMPE UNA ITERACION, EL CONTINUE SALE Y VUELVE A ENTRAR Y NO EJECUTA LO QUE ESTE ABAJO
+	//el FOR puede tener o no condiciones
+	//leer centro numerico o numero perfecto
+	/*var contador;
+	contador=0;
+
+	for(;;)
+	{
+		contador++
+		console.log(contador);
+		if(contador==5)
+		{
+			break;
+		}
+	}
+
+	console.log("final" + contador);*/
+
+	var contador;
+
+	for(contador=0;contador<10;)
+	{
+		contador++
+		console.log(contador);
+	}
+
+	console.log("Final " + contador);
 
 }
--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,5 +1,23 @@
+//al presionar el botón pedir un número. Informar si el numero es PRIMO o no.
 function mostrar()
 {
+	var numeroIngresado;
+	var numerosAnteriores;
+
+
+
+
+
+	numeroIngresado=prompt("Ingrese numero");
+	numeroIngresado=parseInt(numeroIngresado);
+
+	for(numerosAnteriores=numeroIngresado-1;numerosAnteriores>1;numerosAnteriores--)
+	{
+		if(numeroIngresado%numerosAnteriores==0)
+		{
+			contadorDivi
+		}
+	}
 
 
 
