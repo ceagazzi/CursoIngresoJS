bc9502f554a3d43b19bdb1655581fca715f3ef5e
ceagazzi <40808724+ceagazzi@users.noreply.github.com>
2018-08-03 22:02:51 -0300

    Mas parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,14 @@
+//Pedir por prompt el precio y el porcentaje de descuento, mostrar el precio final con descuento por id.
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var precioConDescuento;
 
+	precio=prompt("Ingrese el precio");
+	precio=parseInt(precio);
+	descuento=prompt("Ingrese el descuento");
+	descuento=parseInt(descuento);
+	precioConDescuento=precio*(descuento/100);
+	document.getElementById("elPrecioFinal").value=precioConDescuento;
 }
